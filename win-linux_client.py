8b7c548ff09b334a4218646d19689c12ced6edc6
infinite98 <39892076+infinite98@users.noreply.github.com>
2018-06-08 17:13:58 +0800

    Create win-linux_client.py

---
+++ win-linux_client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+from socket import *
+HOST = '118.228.168.70'       #服务器的地址
+PORT = 20000
+BUFSIZE = 1024
+ADDR = (HOST,PORT)
+tcpCliSock = socket(AF_INET, SOCK_STREAM) 
+tcpCliSock.connect(ADDR)
+while True:
+  data = input('>')
+  if  data == 'exit':
+    break
+  tcpCliSock.send(data.encode())     #发送给服务器的数据
+  data = tcpCliSock.recv(1024).decode()    #接收数据
+  if data == 'exit':
+    break
+  print (data)
+tcpCliSock.close()
